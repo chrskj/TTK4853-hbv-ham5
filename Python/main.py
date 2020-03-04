145423fc56b0d9105f97dcd561d42cec7fcd6683
Dino School <dinossht@stud.ntnu.no>
2020-03-04 14:39:18 +0100

    add: input files dataset, utils

--- Python/main.py
+++ Python/main.py
@@ -1,43 +1,25 @@
 import numpy as np
 import pandas as pd
 import glob
+from utils import calc_nse
 
-
-# From https://github.com/kratzert/pangeo_lstm_example/blob/master/LSTM_for_rainfall_runoff_modelling.ipynb
-def calc_nse(obs: np.array, sim: np.array) -> float:
-    """Calculate Nash-Sutcliff-Efficiency.
-
-    :param obs: Array containing the observations
-    :param sim: Array containing the simulations
-    :return: NSE value.
-    """
-    # only consider time steps, where observations are available
-    sim = np.delete(sim, np.argwhere(obs < 0), axis=0)
-    obs = np.delete(obs, np.argwhere(obs < 0), axis=0)
-
-    # check for NaNs in observations
-    sim = np.delete(sim, np.argwhere(np.isnan(obs)), axis=0)
-    obs = np.delete(obs, np.argwhere(np.isnan(obs)), axis=0)
-
-    denominator = np.sum((obs - np.mean(obs)) ** 2)
-    numerator = np.sum((sim - obs) ** 2)
-    nse_val = 1 - numerator / denominator
-
-    return nse_val
 
 # Read all input txt files
 path = '../Input files (.txt)'
 all_files = glob.glob(path + "/*.txt")
 
 df_dict = {}
+num_files = 0
 for file_path in all_files:
-    #print(f'Reading file: {file_path}')
+    print(f'Reading file: {file_path}')
     # Name is formatted `./Input files (.txt)/nve_inp_XX.txt`
     number = int(file_path.split('_')[-1].split('.')[0])
 
     df = pd.read_csv(file_path, encoding='cp1252', skiprows=[0], delimiter=r"\s+", parse_dates=[['dd.mm.yyyy',  'hh:mm:ss']])
     df = df.rename(columns={"dd.mm.yyyy_hh:mm:ss": "timestamp"})
     df_dict[number] = df
+    num_files += 1
+print(num_files)
 
 
 print(df_dict[1].keys())
---
+++ Python/utils.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+
+# From https://github.com/kratzert/pangeo_lstm_example/blob/master/LSTM_for_rainfall_runoff_modelling.ipynb
+def calc_nse(obs: np.array, sim: np.array) -> float:
+    """Calculate Nash-Sutcliff-Efficiency.
+
+    :param obs: Array containing the observations
+    :param sim: Array containing the simulations
+    :return: NSE value.
+    """
+    # only consider time steps, where observations are available
+    sim = np.delete(sim, np.argwhere(obs < 0), axis=0)
+    obs = np.delete(obs, np.argwhere(obs < 0), axis=0)
+
+    # check for NaNs in observations
+    sim = np.delete(sim, np.argwhere(np.isnan(obs)), axis=0)
+    obs = np.delete(obs, np.argwhere(np.isnan(obs)), axis=0)
+
+    denominator = np.sum((obs - np.mean(obs)) ** 2)
+    numerator = np.sum((sim - obs) ** 2)
+    nse_val = 1 - numerator / denominator
+
+    return nse_val
